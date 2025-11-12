3f6c3df91c43eccd677cb201f7065a7e752022bf
paresh2421 <pareshnair2411@gmail.com>
2025-11-12 17:25:28 +0530

    first commit

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,17 @@
+const FormInput = ({ label, name, type, defaultValue }) => {
+  return (
+    <div className="form-control">
+      <label htmlFor="" className="label">
+        <span className="label-text capitalize">{label}</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        className="input input-bordered"
+        value={defaultValue}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,7 @@
+const Landing = () => {
+
+    
+ return <h1 className='text-4xl'>Landing</h1>;
+}
+
+export default Landing;
