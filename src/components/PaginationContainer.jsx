3185d9f1c9969cdae77f49682ff9161f7e070e38
paresh2421 <pareshnair2411@gmail.com>
2025-12-13 12:24:15 +0530

    adds full pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,14 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
 const PaginationContainer = () => {
   const { meta } = useLoaderData();
-  // console.log(meta.pagination);
   const { pageCount, page } = meta.pagination;
   const pages = Array.from({ length: pageCount }, (_, index) => {
     return index + 1;
   });
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
   const handlePageChange = (pageNum) => {
-    console.log(pageNum);
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNum);
+    navigate(`${pathname}?${searchParams.toString()}`);
   };
   if (pageCount < 2) return null;
   return (
@@ -16,16 +21,19 @@ const PaginationContainer = () => {
       <div className="join">
         <button
           className="btn btn-xs sm:btn-md join-item"
-          onClick={() => handlePageChange("prev")}
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
         >
           Prev
         </button>
         {pages.map((pageNumber) => {
-          // console.log(pageNumber);
-
           return (
             <button
               key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
               className={`btn btn-xs sm:btn-md border-none join-item ${
                 pageNumber === page ? "bg-base-300 border-base-300" : ""
               }`}
@@ -36,7 +44,11 @@ const PaginationContainer = () => {
         })}
         <button
           className="btn btn-xs sm:btn-md join-item"
-          onClick={() => handlePageChange("next")}
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = 1;
+            handlePageChange(nextPage);
+          }}
         >
           Next
         </button>
