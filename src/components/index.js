16e6715983177f343626285febd472dc2f40d5c7
paresh2421 <pareshnair2411@gmail.com>
2025-12-27 14:50:05 +0530

    orders page updated

--- src/components/index.js
+++ src/components/index.js
@@ -15,3 +15,5 @@ export { default as CartItemsList } from './CartItemsList';
 export { default as CartTotals } from './CartTotals';
 export { default as CartItem } from './CartItem';
 export { default as CheckoutForm } from './CheckoutForm';
+export { default as ComplexPaginationContainer } from './ComplexPaginationContainer';
+export { default as OrdersList } from './OrdersList';
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,53 @@
+import { toast } from "react-toastify";
+import { customFetch } from "../utils";
+import { redirect, useLoaderData } from "react-router-dom";
+import { ComplexPaginationContainer, PaginationContainer, SectionTitle } from "../components";
+import OrdersList from "../components/OrdersList";
+
+export const loader = (store) => async ({ request }) => {
+  const user = store.getState().userState.user;
+
+  if (!user) {
+    toast.warn("You must be logged in to view orders");
+    return redirect("/login");
+  }
+
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  try {
+    const response = await customFetch.get("/orders", {
+      params,
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    console.log(response);
+    
+    return { orders: response.data.data, meta: response.data.meta };
+  } catch (error) {
+    console.log(error);
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      "there was an error accessing your orders";
+
+    toast.error(errorMessage);
+    if (error?.response?.status === 401 || 403) return redirect("/login");
+
+    return null;
+  }
+};
+
 const Orders = () => {
-return <h1 className='text-4xl'>Orders</h1>;
-}
+    const {meta} = useLoaderData();
+    if(meta.pagination.total < 1){
+        return <SectionTitle text='Please make an order' />;
+    }
+  return <>
+        <SectionTitle text='Your Orders' />
+        <OrdersList />
+        <ComplexPaginationContainer />
+  </>;
+};
 
 export default Orders;
